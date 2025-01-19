4fbe532597f90968b5781ae4d0e4b97ea1651367
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-01-20 01:57:36 +0500

    add: basics of python like dataypes, functions and conditional statements

--- 01_Basics_Introduction/index.py
+++ 01_Basics_Introduction/index.py
@@ -4,3 +4,37 @@ def print_number(x):
     print(x);
 
 print_number(2)
+
+number_1 = 10;
+print(type(number_1))
+print(number_1)
+
+case = True;
+print(type(case))
+if case:
+    print("This case is true")
+else:
+    print("This case is false")
+
+
+name = 'Hisham';
+print(name)
+print(type(name))
+for i in name:
+    if i == 'i':
+        continue
+    elif i == 'H':
+        i = i.lower()
+    print(i)
+print(i)
+
+my_name = 'Hisham';
+my_name = "Shah"
+print(my_name)
+
+x_number = 10;
+y_number = x_number;
+print(f"X-number: {x_number} And Y-number: {y_number}")
+
+x_number = 15;
+print(f"X and Y numbers after changing the value of x_number X-number {x_number} and Y-number {y_number}")
